ef05c71bade3564efcac89f0e41e93d69d1bca78
Andrei Matei <andrei@cockroachlabs.com>
2023-05-28 18:57:35 -0400

    bisect results

--- go-debuginfo-examples/no-loclists-on-go120.go
+++ go-debuginfo-examples/no-loclists-on-go120.go
@@ -1,14 +1,18 @@
 package main
 
-//go:noinline
-func foo() {
-	var xx [][5000]byte
-	var x1 [5000]byte
-	x1[4000] = 0xff
-	xx = append(xx, x1)
+type T struct {
+	// The size of the array matters; only 1 element is not enough for the demo.
+	x [2]byte
 }
 
 //go:noinline
+func foo() T {
+	// t1 has a loclist before a74e5f584e
+	var t1, t2 T
+	_ = t2 // t2 is necessary for the demo; without it t1 is completely optimized out?
+	return t1
+}
+
 func main() {
 	foo()
 }
